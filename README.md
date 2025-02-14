7e30e9233ecdc008e258bfa7aa416baca22b31e2
UsoltsevI <usoltsev.ia@phystech.edu>
2025-02-14 15:42:30 +0300

    project frame

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple storage project
+
---
+++ src/main/java/org/example/sstorage/SStorage.java
@@ -0,0 +1,11 @@
+package org.example.sstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SStorage {
+    public static void main(String[] args) {
+        SpringApplication.run(SStorage.class, args);
+    }
+}
